80ec6e22cb9e327b541818aca72b812dba3ce895
Joanna Bell <jjbdigital@gmail.com>
2022-08-10 10:27:02 -0500

    commit tree 60d9cb4c64a19966884ea7a1b7495dc57620c1b6

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(mondayActivity = 'go to the office') {
+    return `This Monday, I will ${mondayActivity}.`
+}
+
+function wrapAdjective(wrapper = "*") {
+    let message = "You are"
+    return function innerFunction(inner = "special") {
+        return `${message} ${wrapper}${inner}${wrapper}!`
+    }
+}
+
+
